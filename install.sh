84f078b25f0db8979a139f6a0f19e9921ccafebc
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-11 11:47:14 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo [*] This Script will install all the required Dependencies....
+apt install python3-pip
+pip3 install termcolor
+pip3 install paramiko
+echo [*]All Dependencies installed Successfully...!
+
