9521491a010e2114e24803eb705ceded0a491b12
SxNade <73271976+SxNade@users.noreply.github.com>
2021-01-17 12:46:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo [+] Thor By SxNade.......  https://github.com/SxNade/Thor
 echo [*] This Script will install all the required Dependencies....
+ehco [*] HOLD ON........!
 sudo apt install python3-pip
 sudo pip3 install termcolor
 sudo pip3 install paramiko
